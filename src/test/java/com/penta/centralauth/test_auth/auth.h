df5d257caa0a7d2ceb39f530ab3d1f6b89e19ba0
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 03:45:51 +0900

    Update auth header

--- src/test/java/com/penta/centralauth/test_auth/auth.h
+++ src/test/java/com/penta/centralauth/test_auth/auth.h
@@ -12,3 +12,8 @@ struct CanMsgBuf
 extern uint8_t Tx_Buffer[64];
 
 void CAN_TJA1043T_Enable(void);
+
+struct idsCanMsgBuf *idsCanRecv(void);
+void idsSendCanData(uint8_t inst, uint32_t mailbox, uint32_t messageId, uint8_t * data, uint32_t len);
+
+#endif
