8fc6c3b99dd08e9c4d493174dfb5c21b9a20d7ce
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 03:38:27 +0900

    update auth can

--- src/test/java/com/penta/centralauth/test_auth/auth.c
+++ src/test/java/com/penta/centralauth/test_auth/auth.c
@@ -6,3 +6,12 @@ flexcan_data_info_t RXdataInfo =
 		.fd_enable   = true,
 		.fd_padding  = 0U
 };
+
+flexcan_data_info_t RXdataInfo64 =
+{
+		.data_length = 64U,
+		.msg_id_type = FLEXCAN_MSG_ID_STD,
+		.enable_brs  = true,
+		.fd_enable   = true,
+		.fd_padding  = 0U
+};
